575c58105e481b40ee5f8b864bbb660285471fc6
ilya-deriv <ilya@deriv.com>
2022-06-06 17:05:48 +0300

    Init

---
+++ lib/presentation/categories_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+      body: Column(),
+    );
+  }
+}
