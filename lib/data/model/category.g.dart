c01790eb88ce0dfd397b4044836b40007679f513
ilya-deriv <ilya@deriv.com>
2022-06-09 14:55:55 +0300

    added icons to addCategory function

--- lib/data/model/category.g.dart
+++ lib/data/model/category.g.dart
@@ -8,7 +8,7 @@ part of 'category.dart';
 
 class CategoryAdapter extends TypeAdapter<Category> {
   @override
-  final int typeId = 0;
+  final int typeId = 1;
 
   @override
   Category read(BinaryReader reader) {
@@ -20,19 +20,22 @@ class CategoryAdapter extends TypeAdapter<Category> {
       name: fields[0] as String,
       description: fields[1] as String,
       totalAmount: fields[2] as int,
+      iconData: fields[3] as int,
     );
   }
 
   @override
   void write(BinaryWriter writer, Category obj) {
     writer
-      ..writeByte(3)
+      ..writeByte(4)
       ..writeByte(0)
       ..write(obj.name)
       ..writeByte(1)
       ..write(obj.description)
       ..writeByte(2)
-      ..write(obj.totalAmount);
+      ..write(obj.totalAmount)
+      ..writeByte(3)
+      ..write(obj.iconData);
   }
 
   @override
